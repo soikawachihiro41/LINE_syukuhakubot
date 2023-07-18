0f63389d201ea696fe49849d1adad5ed1ee04484
soikawa <soikawa0108@gmail.com>
2023-07-19 00:16:38 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'hello/index'
+  post 'callback' => 'line_bot#callback'
+end
